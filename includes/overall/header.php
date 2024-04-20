614aa11396abd60ab7e1b3c6a4174c05b2e3492a
jimmyp80 <jpalferman@gmail.com32905811+jimmyp80@users.noreply.github.com>
2024-04-20 15:16:26 +0100

    Adding template files

---
+++ includes/overall/header.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'includes/head.php'; ?>
+<body class="g-sidenav-show  bg-gray-200">
+    <?php include 'includes/sidebar.php'; ?>
+    <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+        <?php include 'includes/navbar.php'; ?>
