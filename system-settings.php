a03d6c21f337c8e694a951cdea23cbc9eeeec06d
jimmyp80 <jpalferman@gmail.com32905811+jimmyp80@users.noreply.github.com>
2024-04-20 18:21:12 +0100

    Further updates to the dashboard setup

---
+++ system-settings.php
@@ -0,0 +1,10 @@
+<?php include 'includes/overall/header.php'; ?>
+    <div class="container-fluid py-4">
+        <div class="row min-vh-80 h-100">
+            <div class="col-12">
+                <h1>System Settings</h1>
+            </div>
+        </div>
+        <?php include 'includes/footer.php'; ?>
+    </div>
+<?php include 'includes/overall/footer.php'; ?>
